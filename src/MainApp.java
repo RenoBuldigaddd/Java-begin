9cfa62691cdd4eb42bf3405e20fe972352e9affe
Anonymous <user@rivne.itstep.org>
2020-01-18 09:55:34 +0200

    first commit

---
+++ src/MainApp.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class MainApp {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		List<String> list = new ArrayList<String>();
+		list.add("one");
+		list.add("two");
+		list.add("three");
+	}
+}
