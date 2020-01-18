8c16ff6779fe0457d1b4c31201ce1a8c3444ab42
Anonymous <user@rivne.itstep.org>
2020-01-18 10:12:07 +0200

    Plussing 88 to string

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,6 +5,12 @@ public class MainApp {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
+		//ArrayList();
+		ArrayListModivy();
+		}
+	
+	private static void ArrayList() {
+		
 		List<String> list = new ArrayList<String>();
 		list.add("one");
 		list.add("two");
@@ -19,5 +25,24 @@ public class MainApp {
 		}
 		String item = list.get(1);
 		System.out.printf("Item index 1: %s\n",item);
+		
+		for(String str : list) {
+			System.out.println(str);
+		}
+	}	
+
+
+	private static void ArrayListModivy()
+	{
+		List<StringBuilder> lst = new ArrayList<StringBuilder>();
+		lst.add(new StringBuilder("alpha"));
+		lst.add(new StringBuilder("beta"));
+		lst.add(new StringBuilder("charlie"));
+		System.out.println(lst); // [alpha, beta, charlie]
+
+		for (StringBuilder sb : lst) {
+		sb.append("88"); // can modify "mutable" objects
+		}
+		System.out.println(lst); // [alpha88, beta88, charlie88]
 	}
 }
